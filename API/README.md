41c63545d28b81f426eb76d2163954faf980e8d6
fer39e4f <fer39e4f@duck.com>
2023-03-04 14:06:39 -0700

    no changes

--- API/README.md
+++ API/README.md
@@ -1,4 +1,81 @@
-# MDEASM
- MD External Attack Surface Management API helpers and examples.
+# MDEASM API
+MD External Attack Surface Management API helpers and examples.
 
- All these are based on the mdeasm.py helper. The easiest way to import mdeasm.py into the python scripts and jupyter notebooks is to place it in the same directory.
+All these are based on the mdeasm.py helper. It is easiest to import it into the use case scripts and jupyter notebook if it is in the same directory.
+
+### Initialize your mdeasm.Workspaces object:
+```
+import mdeasm
+```
+```
+easm = mdeasm.Workspaces(  
+    workspace_name=<workspace_name>,  
+    tenant_id=<tenant_id>,  
+    subscription_id=<subscription_id>,  
+    client_id=<client_id>,  
+    client_secret=<client_secret>)
+```
+### Interact with MDEASM Workspaces
+```
+easm.get_workspaces()
+```
+```
+easm.create_workspace(  
+    resource_group_name=<resource_group>,  
+    location=<oneOf---easm._locations>,  
+    workspace_name=<new_workspace_name>)
+```
+### Retrieve MDEASM Workspace Assets and Risk Findings
+```
+easm.get_workspace_assets(  
+    query_filter='state = "confirmed" AND kind = "domain"',  
+    asset_list_name='owned_domains',  
+    get_all=True)
+```
+```
+easm.get_workspace_assets(  
+    query_filter='state = "confirmed" AND kind = "host" AND ipAddress empty AND cname !empty',  
+    asset_list_name='hosts_with_cnames',  
+    get_all=True)
+```
+```
+easm.get_workspace_asset_by_id(  
+    asset_id='domain$$mydomain.com')
+```
+```
+easm.get_workspace_risk_observations(  
+    severity=<low,med,high>)
+```
+### Interrogate Asset details
+```
+easm.asset_lists()
+```
+>hosts_with_cnames  
+owned_domains
+
+```
+easm.hosts_with_cnames.assets[0].to_dict()
+```
+```
+easm.owned_domains.assets[0].pretty()
+```
+```
+easm.facet_filters()  
+```
+>kind  
+host  
+domain  
+headers  
+...etc...
+```
+easm.filters.host
+```
+```
+easm.filters.headers
+```
+```
+easm.query_facet_filter(  
+    search=<search_term>,  
+    out_format=<print,csv,json>,  
+    out_path=<only_used_with_csv_or_json>)
+```
---
+++ API/hosts_with_CNAME_no_IP_possible_subdomain_takeover.py
@@ -0,0 +1,44 @@
+import mdeasm
+
+# name of the EASM resource
+workspace_name = ''
+
+tenant_id = ''
+subscription_id = ''
+
+# service principal needs to have Contributor permissions on EASM resource
+client_id = ''
+client_secret = ''
+
+easm = mdeasm.Workspaces(workspace_name=workspace_name, tenant_id=tenant_id, subscription_id=subscription_id, client_id=client_id, client_secret=client_secret)
+
+#query for hosts with at least one CNAME record but no live IP Address
+hosts_with_cnames = 'state = "confirmed" AND kind = "host" AND ipAddress empty AND cname !empty'
+#query for domains owned by the org
+owned_domains = 'state = "confirmed" AND kind = "domain"'
+
+#retreive assets for each of the query filters
+#if we want to automatically extract all unique attribute values and associated assets, we would not change auto_create_facet_filters from its default value True (`auto_create_facet_filters=True`)
+easm.get_workspace_assets(query=hosts_with_cnames, asset_list_name='hosts_with_cnames', max_page_size=100, get_all=True, auto_create_facet_filters=False)
+
+#since we only care about the CNAME attribute and values, we can disable auto_create_facet_filters for all attributes in the above function, and instead explicitly create the CNAME facet filter
+easm.create_facet_filter(asset_list_name='hosts_with_cnames', attribute_name='cnames')
+
+#we don't need to create any facet filters for this either, as all we care about are the domain name values
+easm.get_workspace_assets(query=owned_domains, asset_list_name='owned_domains', max_page_size=100, get_all=True, auto_create_facet_filters=False)
+
+#create the list of domain names
+domain_list = []
+for asset in easm.owned_domains.assets:
+    domain_list.append(asset.name)
+
+#compare the CNAME record values in the facet filter with the list of domain names
+#and only keep (print) those where the CNAME record does not end with a domain owned by the Org
+for cname,assetlist in easm.filters.cnames.items():
+    if not cname[0].endswith(tuple(domain_list)):
+        print('CNAME record:', cname[0])
+        print('Assets with CNAME record:', ','.join(assetlist['assets']).replace('host$$',''))
+        #include an empty line after each result set just for a bit easier viewing of output
+        print()
+        #alternative printing if its preferred to keep the Asset Type indicator (host$$) with the asset name
+        #print('Assets with CNAME record:', ','.join(assetlist['assets']))
